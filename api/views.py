a6c7ed10ebe5ad09ce2ffe16dc00e3cd14974c66
SheldonWang <naozhong@gmail.com>
2019-11-18 13:55:49 -0500

    remove authentication and user for temporary and add markdown for content

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
+from markdown2 import Markdown
 from posts.models import Post
 from django.utils import timezone
+from rest_framework import status
 
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import (
@@ -31,9 +33,18 @@ def api_root(request, format=None):
 class PostCreateViewSet(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateSerializer
-    permission_classes = [IsAuthenticated]
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def post(self, request, *args, **kwargs):
+        custom_data = request.data
+        markdowner = Markdown()
+        custom_data['content'] =  markdowner.convert(custom_data['content'])
+
+        serializer = PostCreateSerializer(data=custom_data)
+        if serializer.is_valid():
+            serializer.save()
+            return super(PostCreateViewSet, self).create(request, args, kwargs)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
 
 class PostListViewSet(ListAPIView):
     queryset = Post.objects.all().order_by('-publish_date')
@@ -45,7 +56,7 @@ class PostListViewSet(ListAPIView):
 class PostDetailViewSet(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
     def put(self, request, *args, **kwargs):
         obj = self.get_object()
         new_obj = request.data
--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1) 
     title = models.CharField(max_length=200)
     content = models.TextField()
     publish_date = models.DateTimeField(auto_now_add=True, auto_now=False)
