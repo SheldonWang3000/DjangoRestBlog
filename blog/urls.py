e2a2a39bc7daab5ee8729e2c11db854255e670de
SheldonWang <naozhong@gmail.com>
2019-11-26 09:05:10 -0500

    change url root to depoly on nginx

--- blog/urls.py
+++ blog/urls.py
@@ -23,10 +23,10 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('api/v1/', include('api.urls', namespace='api_v1')),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls)
+    path('v1/', include('api.urls', namespace='api_v1')),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('admin/', admin.site.urls)
 ]
