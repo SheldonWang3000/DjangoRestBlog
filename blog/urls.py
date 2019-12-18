a4f67b23b2ea4793ba7df4a11fb3bbde8e27313e
SheldonWang <naozhong@gmail.com>
2019-12-18 12:28:01 -0500

    change token view

--- blog/urls.py
+++ blog/urls.py
@@ -17,15 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+    TokenObtainPairInCookiesView,
+    TokenRefreshInCookiesView,
     TokenVerifyView,
 )
 
 urlpatterns = [
     path('api/v1/', include('api.urls', namespace='api_v1')),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/', TokenObtainPairInCookiesView.as_view(), name='token_obtain_pair'),
+    path('api/v1/refresh/', TokenRefreshInCookiesView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     # path('admin/', admin.site.urls)
